3adf7e04ac16e94792ea3804f420c036b0f7ad62
sector032 <szantodavid032@gmail.com>
2021-01-05 10:39:43 +0100

    navbar, header complete

---
+++ src/components/AlreadyWatched.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function AlreadyWatched() {
+	return (
+		<div style={mainStyle}>
+			<h2>Already Watched</h2>
+			<p>Teszt5</p>
+		</div>
+	);
+}
+
+const mainStyle = {
+	textAlign: 'center',
+};
