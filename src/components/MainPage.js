ad326267e9680c5ab63125d835175980f2c0e0a9
belbanas <belbanas@gmail.com>
2021-01-05 13:43:10 +0100

    onchange handler

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,20 +1,29 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
 const SearchBarStyle = styled.div`
-	font-weight: bold;
-	text-align: center;
+    font-weight: bold;
+    text-align: center;
 `;
 
 const MainPage = (props) => {
-	return (
-		<React.Fragment>
-			<SearchBarStyle>
-				<input type='text' placeholder='Search a movie or Tv Show'></input>
-				teszt
-			</SearchBarStyle>
-		</React.Fragment>
-	);
+    const handleSearch = (event) => {
+        console.log(event.target.value);
+    };
+
+    return (
+        <React.Fragment>
+            <SearchBarStyle>
+                <input
+                    name="search"
+                    type="text"
+                    placeholder="Search a movie or Tv Show"
+                    onChange={handleSearch}
+                />
+                teszt
+            </SearchBarStyle>
+        </React.Fragment>
+    );
 };
 
 export default MainPage;
