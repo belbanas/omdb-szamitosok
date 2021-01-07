487e77e494688e04ef1fe85cb8d1da98888adf96
sector032 <szantodavid032@gmail.com>
2021-01-07 11:47:30 +0100

    need to fix

--- src/components/AddMovie.js
+++ src/components/AddMovie.js
@@ -1,30 +1,58 @@
-import React, { useContext } from "react";
-import { MovieContext } from "./MovieContext";
-import styled from "styled-components";
+import React, { useContext } from 'react';
+import { MovieContext } from './MovieContext';
+import styled from 'styled-components';
 
 const Button = styled.button`
-    padding: 0.5rem;
-    margin: 0.5rem;
+	padding: 0.5rem;
+	margin: 0.5rem;
 `;
 
 const AddMovie = (props) => {
-    const [movies, setMovies] = useContext(MovieContext);
+	const [movies, setMovies] = useContext(MovieContext);
 
-    const addMovie = () => {
-        if (!movies.watchlist.includes(props.watchlist)) {
-            setMovies({
-                watchlist: [...movies.watchlist, props.watchlist],
-            });
-        } else {
-            alert("Already added!");
-        }
-    };
+	const addMovie = () => {
+		if (movies.watchlist.length > 0) {
+			for (let movie of movies.watchlist) {
+				if (movie.Title === props.movie.Title) {
+					alert('Already kaki');
+				} else {
+					setMovies({
+						watchlist: [...movies.watchlist, props.watchlist],
+						alreadyWatched: [...movies.alreadyWatched],
+					});
+					console.log('kakkanat');
+				}
+			}
+		} else {
+			setMovies({
+				watchlist: [...movies.watchlist, props.watchlist],
+				alreadyWatched: [...movies.alreadyWatched],
+			});
+		}
+	};
 
-    return (
-        <Button type="submit" onClick={addMovie}>
-            Add to Watchlist
-        </Button>
-    );
+	const addMovieTwo = () => {
+		if (!movies.alreadyWatched.includes(props.movie.Title)) {
+			setMovies({
+				watchlist: [...movies.watchlist],
+				alreadyWatched: [...movies.alreadyWatched, props.movie.Title],
+			});
+		} else {
+			alert('Already added!');
+		}
+	};
+
+	return (
+		<React.Fragment>
+			<Button type='submit' onClick={addMovie}>
+				Add to Watchlist
+			</Button>
+
+			<Button type='submit' onClick={addMovieTwo}>
+				Seen
+			</Button>
+		</React.Fragment>
+	);
 };
 
 export default AddMovie;
--- src/components/WatchList.js
+++ src/components/WatchList.js
@@ -1,39 +1,39 @@
-import React, { useState, useContext } from "react";
-import { MovieContext } from "./MovieContext";
-import Item from "./Item";
-import styled from "styled-components";
+import React, { useState, useContext } from 'react';
+import { MovieContext } from './MovieContext';
+import Item from './Item';
+import styled from 'styled-components';
 
 const MovieCards = styled.div`
-    display: flex;
-    flex-flow: row wrap;
-    justify-content: center;
+	display: flex;
+	flex-flow: row wrap;
+	justify-content: center;
 `;
 
 const Movies = () => {
-    const [movies, setMovies] = useContext(MovieContext);
+	const [movies, setMovies] = useContext(MovieContext);
 
-    const getWatchlist = () => {
-        if (movies.watchlist.length >= 0) {
-            return movies.watchlist.map((item) => (
-                <Item
-                    key={item.imdbID}
-                    title={item.Title}
-                    year={item.Year}
-                    type={item.Type}
-                    poster={item.Poster}
-                    imdbid={item.imdbID}
-                />
-            ));
-        } else {
-            return <h1>Nincs</h1>;
-        }
-    };
+	const getWatchlist = () => {
+		if (movies.watchlist.length >= 0) {
+			return movies.watchlist.map((item) => (
+				<Item
+					key={item.imdbID}
+					title={item.Title}
+					year={item.Year}
+					type={item.Type}
+					poster={item.Poster}
+					imdbid={item.imdbID}
+				/>
+			));
+		} else {
+			return <h1>Nincs</h1>;
+		}
+	};
 
-    return (
-        <div>
-            <MovieCards>{getWatchlist()}</MovieCards>
-        </div>
-    );
+	return (
+		<div>
+			<MovieCards>{getWatchlist()}</MovieCards>
+		</div>
+	);
 };
 
 export default Movies;
