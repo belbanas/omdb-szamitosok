013b6d3e69441e163390e8c2e76426d882366469
belbanas <belbanas@gmail.com>
2021-03-08 13:38:58 +0100

    change api call for imdb id search

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,149 +1,153 @@
-import React, { useState, useEffect, useContext } from 'react';
-import styled from 'styled-components';
-import Flippy, { FrontSide, BackSide } from 'react-flippy';
-import axios from 'axios';
-import AddMovie from './AddMovie';
-import { MovieContext } from './MovieContext';
+import React, { useState, useEffect, useContext } from "react";
+import styled from "styled-components";
+import Flippy, { FrontSide, BackSide } from "react-flippy";
+import axios from "axios";
+import AddMovie from "./AddMovie";
+import { MovieContext } from "./MovieContext";
 
 const MovieCard = styled.div`
-	color: black;
-	padding: 5px 5px;
-	border: 3px solid;
-	background: lightgrey;
-	text-align: center;
-	border-radius: 1rem;
-	width: 22rem;
-	height: 43rem;
-	display: flex;
-	flex-flow: column wrap;
-	justify-content: center;
-	align-content: center;
-	align-items: center;
-	overflow: hidden;
+    color: black;
+    padding: 5px 5px;
+    border: 3px solid;
+    background: lightgrey;
+    text-align: center;
+    border-radius: 1rem;
+    width: 22rem;
+    height: 43rem;
+    display: flex;
+    flex-flow: column wrap;
+    justify-content: center;
+    align-content: center;
+    align-items: center;
+    overflow: hidden;
 `;
 
 const Poster = styled.img`
-	max-width: 110%;
-	max-height: 110%;
+    max-width: 110%;
+    max-height: 110%;
 `;
 
 const CardDetails = styled.div`
-	text-align: center;
-	display: flex;
-	flex-flow: column wrap;
-	justify-content: center;
-	align-content: center;
-	align-items: center;
+    text-align: center;
+    display: flex;
+    flex-flow: column wrap;
+    justify-content: center;
+    align-content: center;
+    align-items: center;
 `;
 
 const Title = styled.h3``;
 
 const Detail = styled.p`
-	padding: 0.01rem;
+    padding: 0.01rem;
 `;
 
 const Button = styled.button`
-	padding: 0.5rem;
-	margin: 0.5rem;
+    padding: 0.5rem;
+    margin: 0.5rem;
 `;
 
 const Score = styled.p`
-	padding: 0.8rem;
-	border: 2px solid;
-	border-radius: 5rem;
-	width: 3.5rem;
-	background: yellow;
-	font-weight: bold;
+    padding: 0.8rem;
+    border: 2px solid;
+    border-radius: 5rem;
+    width: 3.5rem;
+    background: yellow;
+    font-weight: bold;
 `;
 
 const Scores = styled.div`
-	float: left;
-	display: flex;
+    float: left;
+    display: flex;
 `;
 
 const GrayScale = styled.div``;
 
 const Item = (props) => {
-	const [details, setDetails] = useState({});
-	const [movies, setMovies] = useContext(MovieContext);
-	const [grayscale, setGrayscale] = useState(false);
+    const [details, setDetails] = useState({});
+    const [movies, setMovies] = useContext(MovieContext);
+    const [grayscale, setGrayscale] = useState(false);
 
-	useEffect(() => {
-		axios
-			.get('http://www.omdbapi.com/?apikey=530b6ee1&i=' + props.imdbid)
-			.then((response) => {
-				setDetails(response.data);
-				grayScaled();
-			});
-	}, [props.imdbid, movies.alreadyWatched]);
+    useEffect(() => {
+        axios
+            .get("http://localhost:8000/api/movie/" + props.imdbid)
+            .then((response) => JSON.parse(response.data))
+            .then((resp) => {
+                setDetails(resp);
+                grayScaled();
+            });
+    }, [props.imdbid, movies.alreadyWatched]);
 
-	const grayScaleStyle = () => {
-		if (grayscale) {
-			return {
-				filter: 'grayscale(100%)',
-			};
-		} else {
-			return {
-				filter: 'grayscale(0%)',
-			};
-		}
-	};
-	const grayScaled = () => {
-		if (movies.alreadyWatched.includes(props.title)) {
-			setGrayscale(true);
-		} else {
-			setGrayscale(false);
-		}
-	};
+    const grayScaleStyle = () => {
+        if (grayscale) {
+            return {
+                filter: "grayscale(100%)",
+            };
+        } else {
+            return {
+                filter: "grayscale(0%)",
+            };
+        }
+    };
+    const grayScaled = () => {
+        if (movies.alreadyWatched.includes(props.title)) {
+            setGrayscale(true);
+        } else {
+            setGrayscale(false);
+        }
+    };
 
-	const [cardFlip, setCardFlip] = useState(true);
+    const [cardFlip, setCardFlip] = useState(true);
 
-	const changeCardFlip = () => {
-		if (cardFlip) {
-			setCardFlip(false);
-		} else {
-			setCardFlip(true);
-		}
-	};
+    const changeCardFlip = () => {
+        if (cardFlip) {
+            setCardFlip(false);
+        } else {
+            setCardFlip(true);
+        }
+    };
 
-	return (
-		<React.Fragment>
-			<Flippy
-				flipOnHover={false}
-				flipOnClick={cardFlip}
-				flipDirection='horizontal'
-			>
-				<FrontSide>
-					<MovieCard>
-						<GrayScale style={grayScaleStyle()}>
-							<Poster src={details.Poster}></Poster>
-						</GrayScale>
-						<Title>{details.Title}</Title>
-						<p>{details.Year}</p>
-					</MovieCard>
-				</FrontSide>
-				<BackSide>
-					<MovieCard>
-						<CardDetails>
-							<Title>{details.Title}</Title>
-							<Scores>
-								<Score>{details.imdbRating}</Score>
-								<Score>{details.imdbRating}</Score>
-							</Scores>
-							<Detail>{details.Runtime}</Detail>
-							<Detail>{details.Genre}</Detail>
-							<Detail>{details.Plot}</Detail>
-							<Detail>Director: {details.Director}</Detail>
-							<Detail>Starring: {details.Actors}</Detail>
-							<Detail>
-								<AddMovie changeCard={changeCardFlip} movie={details} />
-							</Detail>
-						</CardDetails>
-					</MovieCard>
-				</BackSide>
-			</Flippy>
-		</React.Fragment>
-	);
+    return (
+        <React.Fragment>
+            <Flippy
+                flipOnHover={false}
+                flipOnClick={cardFlip}
+                flipDirection="horizontal"
+            >
+                <FrontSide>
+                    <MovieCard>
+                        <GrayScale style={grayScaleStyle()}>
+                            <Poster src={details.Poster}></Poster>
+                        </GrayScale>
+                        <Title>{details.Title}</Title>
+                        <p>{details.Year}</p>
+                    </MovieCard>
+                </FrontSide>
+                <BackSide>
+                    <MovieCard>
+                        <CardDetails>
+                            <Title>{details.Title}</Title>
+                            <Scores>
+                                <Score>{details.imdbRating}</Score>
+                                <Score>{details.imdbRating}</Score>
+                            </Scores>
+                            <Detail>{details.Runtime}</Detail>
+                            <Detail>{details.Genre}</Detail>
+                            <Detail>{details.Plot}</Detail>
+                            <Detail>Director: {details.Director}</Detail>
+                            <Detail>Starring: {details.Actors}</Detail>
+                            <Detail>
+                                <AddMovie
+                                    changeCard={changeCardFlip}
+                                    movie={details}
+                                />
+                            </Detail>
+                        </CardDetails>
+                    </MovieCard>
+                </BackSide>
+            </Flippy>
+        </React.Fragment>
+    );
 };
 
 export default Item;
